60a1f41645cf38626523ad0ac8dcb043baa78927
Jack Greenberg <theProgrammerJack@gmail.com>
2019-05-23 15:17:42 +0000

    Moved csv functionality into cli

--- knot-solver-cli/src/main.rs
+++ knot-solver-cli/src/main.rs
@@ -1,5 +1,14 @@
-use knot_solver::Knot;
-use std::str::FromStr;
+#[macro_use]
+extern crate serde_derive;
+
+use knot_solver::{Knot, KnotParseError};
+use std::{
+    error::Error,
+    io,
+    path::{Path, PathBuf},
+    str::FromStr,
+    string::ToString,
+};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -18,6 +27,13 @@ enum Command {
 
     #[structopt(name = "jones")]
     Jones { braid: String },
+
+    #[structopt(name = "csv")]
+    Csv {
+        #[structopt(short = "o", long = "output")]
+        output: Option<PathBuf>,
+        braids: Vec<String>,
+    },
 }
 
 fn main() {
@@ -36,5 +52,49 @@ fn main() {
             "{}",
             Knot::from_str(braid.as_str()).unwrap().jones_polynomial()
         ),
+        Command::Csv { output, braids } => run_csv(output, braids).unwrap(),
     }
+}
+
+#[derive(Debug, Serialize)]
+struct KnotRecord {
+    braid: String,
+    bracket: String,
+    beta: String,
+    jones: String,
+}
+
+impl KnotRecord {
+    fn with_braid(braid: String) -> Result<Self, KnotParseError> {
+        let knot = Knot::from_str(braid.as_str())?;
+        Ok(KnotRecord {
+            braid,
+            bracket: knot.bracket_polynomial().to_string(),
+            beta: knot.beta_polynomial().to_string(),
+            jones: knot.jones_polynomial().to_string(),
+        })
+    }
+}
+
+fn run_csv<P: AsRef<Path>>(output_path: Option<P>, braids: Vec<String>) -> Result<(), Box<Error>> {
+    if let Some(path) = output_path {
+        let wtr = csv::Writer::from_path(path)?;
+        write_csv(wtr, braids)
+    } else {
+        let wtr = csv::Writer::from_writer(io::stdout());
+        write_csv(wtr, braids)
+    }
+}
+
+fn write_csv<T: io::Write>(mut wtr: csv::Writer<T>, braids: Vec<String>) -> Result<(), Box<Error>> {
+    for record in braids
+        .into_iter()
+        .map(|braid| KnotRecord::with_braid(braid.to_string()).unwrap())
+    {
+        wtr.serialize(record)?;
+    }
+
+    wtr.flush()?;
+
+    Ok(())
 }
